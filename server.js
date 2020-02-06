bfefd65af3a420454088a97956a384531a4aad49
Abhinav Gupta <abhinav2119@gmail.com>
2020-02-06 16:25:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+// Middleware functions can perform the following tasks:
+
+// Execute any code.
+// Make changes to the request and the response objects.
+// End the request-response cycle.
+// Call the next middleware function in the stack.
+
+const express = require('express');
+const hbs = require('hbs'); //handlebars
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {  // used for middleware 
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log + '\n');
+    next();
+});
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public' )); 
+// this is used after the maintenace.hbs rendering beacuse the help.html which is present will not show us the maintenance page, to avoid this all we do is print the express.static after the maintenance.hbs
+
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear() // it gets the actuall date of the javascript constructor
+});
+
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res) =>{
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        WelcomeMessage: 'Hey there! Welcome to our web  page'
+    })
+});
+
+
+
+// app.get('/',(req,res) => {
+//     // res.send('Hello Express');
+//     // res.send('<h1>Hello Express</h1>');
+//     res.send({
+//         name:'izuku',
+//         likes : [
+//             'Almight',
+//             'Hero'
+//         ]
+//     });
+// });
+
+app.get('/about',(req,res) => {
+    res.render('About.hbs',{
+        pageTitle : 'About Page',
+    }); // Render is gonna let you render any of the templates you have set up with your current view engine
+}); 
+
+app.get('/bad',(req,res) => {
+    res.send({
+        errorMessage : 'Unable to handle request'
+    });
+});
+
+app.listen(3000,() => {
+    console.log('Server is up on port 3000');
+});
